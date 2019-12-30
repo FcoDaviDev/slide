346e20d82bbb876c671811c8abb189217b2aa3d3
FcoDaviDev <davigalmeida@hotmail.com>
2019-12-30 10:08:04 -0300

    navegacao prev e next finalizada

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js'
 
-export default class Slide {
+export class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
@@ -58,7 +58,7 @@ export default class Slide {
 
     changeSlideOnEnd() {
         if(this.dist.movement > 120 && this.index.next !== undefined) {
-            this.activeNexttSlide();
+            this.activeNextSlide();
         } else if(this.dist.movement < -120 && this.index.prev !== undefined) {
             this.activePrevSlide();
         } else {
@@ -115,7 +115,7 @@ export default class Slide {
 
     }
 
-    activeNexttSlide() {
+    activeNextSlide() {
         if(this.index.next != undefined) this.changeSlide(this.index.next);
     }
 
@@ -135,6 +135,9 @@ export default class Slide {
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
 
+        this.activePrevSlide = this.activePrevSlide.bind(this);
+        this.activeNextSlide = this.activeNextSlide.bind(this);
+
         this.onResize = debounce(this.onResize.bind(this), 200);
     }
 
@@ -144,6 +147,21 @@ export default class Slide {
         this.addSlideEvents();
         this.slidesConfig();
         this.addResizeEvent();
+        this.changeSlide(0);
         return this;
     }
-}   
+}  
+
+// --- SLIDE NAV ----
+export class SlideNav extends Slide {
+    addArrow(prev, next) {
+        this.prevElement = document.querySelector(prev);
+        this.nextElement = document.querySelector(next);
+        this.addArrowEvent();
+    }
+
+    addArrowEvent() {
+        this.prevElement.addEventListener('click', this.activePrevSlide);
+        this.nextElement.addEventListener('click', this.activeNextSlide);
+    }
+}
